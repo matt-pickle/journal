3880ebf57adcb602d07c20c0742a2430a9e71201
Matt <mpickle86@gmail.com>
2020-08-05 20:22:32 -0500

    Set up basic client structure

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import '../styles/styles.scss';
+import {Switch, Route, Redirect} from "react-router-dom";
+import {UserContextProvider} from "./UserContext";
+import Main from "./Main";
+import ProtectedRoute from "./ProtectedRoute";
+import Register from "./Register";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/main" />
+        </Route>
+        <ProtectedRoute path="/main">
+          <UserContextProvider>
+            <Main />
+          </UserContextProvider>
+        </ProtectedRoute>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
-  );
+    
+  )
 }
 
 export default App;
---
+++ client/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LogoutButton from "./LogoutButton";
+
+function Main() {
+  return (
+    <div className="main-container">
+      <h1>Hello World</h1>
+      <LogoutButton />
+    </div>
+  )
+}
+
+export default Main;
