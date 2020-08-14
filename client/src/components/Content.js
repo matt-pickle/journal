fd4698b6752543599514dd25123e1f33527a638c
Matt <mpickle86@gmail.com>
2020-08-13 19:17:21 -0500

    Set up conditional rendering of NewEntry or displayed entry

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Button from "./Button";
 import NewEntry from "./NewEntry";
 import "../styles/styles.scss";
 
 function Content(props) {
-
   return (
     <div className="content-container">
       <h1>Content</h1>
-      <NewEntry />
-      <p className="displayed-entry">{props.displayedEntry}</p>
+      <Button text="New Entry" 
+              onClick={props.startNewEntry}
+      />
+      {props.isNewEntryOpen ?
+        <NewEntry /> :
+        <p className="displayed-entry">{props.displayedEntry}</p>
+      }
     </div>
   )
 }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -4,8 +4,9 @@ import Navigation from "./Navigation";
 import Content from "./Content";
 
 function Main() {
-  const {user, journal} = useContext(Context);
+  const {journal} = useContext(Context);
   const [displayedEntry, setDisplayedEntry] = useState("");
+  const [isNewEntryOpen, setIsNewEntryOpen] = useState(false);
 
   //Changes displayed entry when a nav link is clicked
   function displayEntry(entryDate) {
@@ -14,12 +15,21 @@ function Main() {
         return item.date === entryDate;
       })[0].entry
     )
+    setIsNewEntryOpen(false);
+  }
+  
+  //Opens New Entry box when "New Entry" button is clicked
+  function startNewEntry() {
+    setIsNewEntryOpen(true);
   }
 
   return (
     <div className="main-container">
       <Navigation displayEntry={displayEntry}/>
-      <Content displayedEntry={displayedEntry}/>
+      <Content displayedEntry={displayedEntry}
+               isNewEntryOpen={isNewEntryOpen}
+               startNewEntry={startNewEntry}
+      />
     </div>
   )
 }
